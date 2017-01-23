5b81c22bc35b9c88c7870d2a6778f14076d2a8d2
Ivo von Putzer Reibegg <ivo.putzer@gmail.com>
2017-01-24 00:57:42 +0100

    fixes async tests

--- test/asynchronous.js
+++ test/asynchronous.js
@@ -1,26 +1,13 @@
-// const {ok} = require('assert')
-const {test} = require('..')
+const {ok} = require('assert')
 
 test('asynchronous', function () {
   test('runner works!', function (done) {
     setTimeout(() => done(), 0)
   })
   test.skip('runner fails!', function (done) {
-    setTimeout(() => done(Error()), 0)
+    setTimeout(() => done({name: 'AssertionError', message: ''}), 0)
+  }, !process.env.DEBUG)
+  test.skip('runner fails from exception!', function (done) {
+    setTimeout(() => ok(false), 0)
   }, !process.env.DEBUG)
 })
-
-// test.timeout('runner fails async with timeout!', function (done) {
-//   setTimeout(() => done(), 100)
-// }, 50)
-
-// test.timeout('runner works!', function (done) {
-//   setTimeout(() => done(), 100)
-// }, 50, false)
-
-// test.timeout('retrieves google', function (done) {
-//   require('http').get({
-//     host: 'google.com',
-//     path: '/'
-//   }, done.bind(null, null))
-// }, 100)
