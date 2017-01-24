9cc58b2ce683167f8eb6219d885a1062bd12cabb
Ivo von Putzer Reibegg <ivo.putzer@gmail.com>
2017-01-24 01:08:23 +0100

    fixes async tests

--- test/asynchronous.js
+++ test/asynchronous.js
@@ -5,7 +5,7 @@ test('asynchronous', function () {
     setTimeout(() => done(), 0)
   })
   test.skip('runner fails!', function (done) {
-    setTimeout(() => done({name: 'AssertionError', message: ''}), 0)
+    setTimeout(() => done({name: 'AssertionError', message: '', stack: new Error().stack}), 0)
   }, !process.env.DEBUG)
   test.skip('runner fails from exception!', function (done) {
     setTimeout(() => ok(false), 0)
