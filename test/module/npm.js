f65e2d1de9e6da007725abbea21b543a00848209
Ivo von Putzer Reibegg <ivo.putzer@gmail.com>
2017-01-29 00:49:27 +0100

    adds npm tests

---
+++ test/module/npm.js
@@ -0,0 +1,44 @@
+const {equal} = require('assert')
+
+test('module/npm', function () {
+  test('is available on npm!', done => {
+    head({path: process.env.npm_package_name, host: 'registry.npmjs.org', port: 80}, (res) => {
+      equal(res.statusCode, 200)
+      done()
+    })
+  })
+  test.skip('latest version has been deployed!', done => {
+    get({path: process.env.npm_package_name, host: 'registry.npmjs.org', port: 80}, (res) => {
+      equal(res.statusCode, 200)
+      res.pipe(toJson(({'dist-tags': {latest}}) => {
+        equal(latest, process.env.npm_package_version)
+        done()
+      }))
+    })
+  }, !process.env.DEBUG)
+})
+
+function get (options, fn, {assign} = Object) {
+  return require('http').request(assign(options, {
+    method: 'GET',
+    path: '/' + options.path.trimLeft('/')
+  }), fn).end()
+}
+
+function head (options, fn, {assign} = Object) {
+  return require('http').request(assign(options, {
+    method: 'HEAD',
+    path: '/' + options.path.trimLeft('/')
+  }), fn).end()
+}
+
+function toJson (fn, buffer = []) {
+  return require('stream').Writable({
+    write (data, encoding, next) {
+      buffer.push(new Buffer(data, encoding))
+      next()
+    }
+  }).on('finish', ({parse} = JSON, {concat} = Buffer) => {
+    fn(parse(concat(buffer)))
+  })
+}
