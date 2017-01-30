1aaf5aae25d0e9d44be39b16ee034bfd2106921b
Ivo von Putzer Reibegg <ivo.putzer@gmail.com>
2017-01-30 20:45:05 +0100

    moves globals to global

--- test/globals.js
+++ test/globals.js
@@ -1,15 +1,15 @@
 const {ok} = require('assert')
 
-test('globals:', function () {
+test('global:', function () {
   test('exports `test` function!', function () {
-    ok(require('../globals').hasOwnProperty('test'))
+    ok(require('../global').hasOwnProperty('test'))
   })
 
   test('exports `beforeEach` function!', function () {
-    ok(require('../globals').hasOwnProperty('beforeEach'))
+    ok(require('../global').hasOwnProperty('beforeEach'))
   })
 
   test('exports `afterEach` function!', function () {
-    ok(require('../globals').hasOwnProperty('afterEach'))
+    ok(require('../global').hasOwnProperty('afterEach'))
   })
 })
