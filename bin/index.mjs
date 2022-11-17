a4b61ef33e8a697aebddb28cade567fb62418119
Ivo von Putzer Reibegg <ivo.putzer@gmail.com>
2022-11-17 22:02:38 +0100

    refactoring binary

--- bin/index.mjs
+++ bin/index.mjs
@@ -1,37 +1,33 @@
 #!/usr/bin/env node
 import process from 'node:process'
+
+import { spawn } from 'node:child_process'
 import { realpath } from 'node:fs/promises'
 import { dirname, join } from 'node:path'
-import { spawn } from 'node:child_process'
 import { find } from '../lib/index.mjs'
 
-console.log('-- test')
+// parse args, options and stuff here
+const argOptionFailFast = false
+const argOptionFiles = null ?? find('test')
 
+// run all test suites
 try {
-  const require = join(dirname(dirname(await realpath(process.argv[1]))), 'global.js')
-  const files = find('test')
-  const codes = []
-  const children = []
-
-  process.on('beforeExit', (code) => {
-    children.forEach((child) => child.kill())
-    if (codes.some(value => value > 0)) {
-      process.exit(1)
+  const global = join(dirname(dirname(await realpath(process.argv[1]))), 'global.js')
+  const failed = []
+  for await (const path of argOptionFiles) {
+    const code = await new Promise((resolve, reject) => spawn('node', ['--require', global, path], { stdio: 'inherit' }).on('close', resolve).on('error', reject))
+    if (hasFailed(code)) {
+      failed.push(code)
+      if (argOptionFailFast) break
     }
-  })
-
-  for await (const path of files) {
-    children.push(
-      spawn('node', ['--require', require, path], { stdio: 'inherit' })
-        .on('close', code => codes.push(code))
-    )
+  }
+  if (failed.some(hasFailed)) {
+    process.exit(1)
   }
 } catch (error) {
   console.error(error)
 }
 
-// we need to find a way to add:
-// - [ ] watch feature
-// - [ ] parallel execution
-// - [ ] coverage
-// - [ ] debug
+function hasFailed (code) {
+  return code !== 0
+}
